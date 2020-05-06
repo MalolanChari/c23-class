6a708eb9dc01ef139918b2de5804677b6353d939
MalolanChari <64600522+MalolanChari@users.noreply.github.com>
2020-05-06 13:58:09 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# c23-class
+# staticSquare
+A static square made using Matter.js physics engine
---
+++ sketch.js
@@ -0,0 +1,33 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var ground,box1,box2;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    var ground_options ={
+        isStatic: true
+    }
+    
+    box1=new Box (200,200,50,50)
+    box2=new Box (170,100,50,50)
+
+    ground = Bodies.rectangle(200,390,200,20,ground_options);
+    World.add(world,ground);
+    
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    box1.display();
+    box2.display();
+    
+    rectMode(CENTER);
+    rect(ground.position.x,ground.position.y,400,20);
+}
